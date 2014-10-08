c7ce252356d4ab0e3ff6071be4fb40b8fb8111a7
Dan Taeyoung <provolot@gmail.com>
2014-10-08 03:50:17 -0400

    test

--- RiftSketch/js/screen.js
+++ RiftSketch/js/screen.js
@@ -1,21 +1,21 @@
-  document.addEventListener("DOMContentLoaded", function(event) {
+function loadVideo() {
 
-var video = document.querySelector("#videoElement");
- 
-navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia || navigator.oGetUserMedia;
- 
-if (navigator.getUserMedia) {      
-    navigator.getUserMedia({video: true}, handleVideo, videoError);
-}
- 
-function handleVideo(stream) {
-    video.src = window.URL.createObjectURL(stream);
-    console.log(video.src);
-}
- 
-function videoError(e) {
-	console.log(e);
-    // do something
-} 
+	var video = document.querySelector("#videoElement");
+	 
+	navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia || navigator.oGetUserMedia;
+	 
+	if (navigator.getUserMedia) {      
+	    navigator.getUserMedia({video: true}, handleVideo, videoError);
+	}
+	 
+	function handleVideo(stream) {
+	    video.src = window.URL.createObjectURL(stream);
+	    console.log(video.src);
+	}
+	 
+	function videoError(e) {
+		console.log(e);
+	    // do something
+	} 
 
-});
+}
